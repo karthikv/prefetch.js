f4a8cfadc0d1060ba8ffce54320770c564a7af39
Vivek Nair <vnair611@stanford.edu>
2013-04-06 21:52:43 -0700

    Add in the foundLinks hash to the resource scraper to prevent over fetching

--- client/resource-scraper.js
+++ client/resource-scraper.js
@@ -6,24 +6,31 @@ exports.findPrefetchableResources = function() {
   types.forEach(function(type) {
     var prefetchableLinks = [];
     var tags = document.getElementsByTagName(type);
+    var foundLinks = {};
 
     // loop through all resources on the page, 
     // aggregating those that can be prefetched
     tags.forEach(function(tag) {
       // for tags with hyperlinks in href
-      if (utils.isPrefetchable(tag.href)) {
+      var absoluteLink = utils.makeLinkAbsolute(tag.href);
+      if (utils.isPrefetchable(tag.href) && !foundLinks[absoluteLink]) {
         prefetchableLinks.push({
           original: tag.href,
-          absolute: utils.makeLinkAbsolute(tag.href)
-        })
+          absolute: absoluteLink
+        });
+
+        foundLinks[absoluteLink] = true;
       }
 
       // for tags with hyperlinks in src
-      if (utils.isPrefetchable(tag.src)) {
+      var absoluteLink = utils.makeLinkAbsolute(tag.src);
+      if (utils.isPrefetchable(tag.src) && !foundLinks[absoluteLink]) {
         prefetchableLinks.push({
           original: tag.src,
-          absolute: utils.makeLinkAbsolute(tag.src)
-        })
+          absolute: absoluteLink
+        });
+
+        foundLinks[absoluteLink] = true;
       }
     });
 
