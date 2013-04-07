24b2ddcc75c5e0dbb192703c593f4bfa5311c436
Karthik Viswanathan <me@karthikv.net>
2013-04-06 20:46:26 -0700

    Connect all modules together in main.js.

--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 (function(window, document, undefined) {
   // TODO: check if filesystem API is supported
-  var scraper = require('./scraper.js');
-  var fs = require('./fs.js');
-  
-  
- })(this, this.document);
+  var linkScraper = require('./link-scraper.js');
+  var linkPrefetcher = require('./link-prefetcher.js');
+  var resourceScraper = require('./resource-scraper.js');
+  var resourcePrefetcher = require('./resource-prefetcher.js');
+  var resourceReplacer = require('./resource-replacer.js');
 
-// Port 1875
+  // prefetch all links on the page
+  var links = linkScraper.findPrefetchableLinks();
+  linkPrefetcher.prefetch(links, function(url, body) {
+    // prefetch all resources corresponding to those links
+    var resources = resourceScraper.findPrefetchableResources(body);
+
+    resourcePrefetcher.prefetch(resources, function(fsURL) {
+      // rewrite URLs dynamically
+      body = resourcePrefetcher.rewrite(body, url, fsURL);
+    });
+  });
+})(this, this.document);
