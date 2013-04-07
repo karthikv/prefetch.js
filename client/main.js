a95d07215d288cdb4553c9648b3505dfa83f1906
Karthik Viswanathan <me@karthikv.net>
2013-04-06 21:17:41 -0700

    Lay out structure for final link prefetch handler.

--- client/main.js
+++ client/main.js
@@ -5,6 +5,10 @@
   var resourceScraper = require('./resource-scraper.js');
   var resourcePrefetcher = require('./resource-prefetcher.js');
   var resourceReplacer = require('./resource-replacer.js');
+  var utils = require('./utils.js');
+
+  // mapping from link to response body 
+  var linkToResponseBody = {};
 
   // prefetch all links on the page
   var links = linkScraper.findPrefetchableLinks();
@@ -15,6 +19,21 @@
     resourcePrefetcher.prefetch(resources, function(fsURL) {
       // rewrite URLs dynamically
       body = resourcePrefetcher.rewrite(body, url, fsURL);
+      linkToResponseBody[url] = body;
     });
+  });
+
+  window.addEventListener('click', function(event) {
+    var target = event.target;
+    if (target.nodeName == 'A') {
+      // has link been prefetched?
+      if (utils.isPrefetchable(target.href) &&
+          target.href in linkToResponseBody) {
+        // prefetchable link was clicked
+        var body = linkToResponseBody[url];
+        // TODO: handle response
+        console.log('link clicked', target.href, 'got data', body);
+      }
+    }
   });
 })(this, this.document);
