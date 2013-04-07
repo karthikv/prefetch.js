c96ebd62892417a6194ca2fe4b563301e246a78c
Vivek Nair <vnair611@stanford.edu>
2013-04-06 21:00:26 -0700

    Modified the prefetcher to have rewrite and prefetch

--- client/resource-prefetcher.js
+++ client/resource-prefetcher.js
@@ -1,9 +1,41 @@
 var utils = require('./utils'); 
 var scraper = require('./resource-scraper');
+var socket = io.connect('http://localhost:1875');
 
 var filer = new Filer();
 filer.init();
 
-exports.prefetchResources = function(localLink) {
+exports.rewrite = function(url, fsurl, body) {
+  // Are these all the resource tags?
+  var types = ['img', 'script', 'link'];
+
+  types.forEach(function(type) {
+    var elements = body.getElementsByTagName(type);
+    elements.forEach(function(element) {
+      if (element.src == url) {
+        element.src = fsurl;
+      }
+  
+      if (element.href == url) {
+        element.href = fsurl;
+      }
+    }); 
+  });
+};
+
+exports.prefetch = function(links, callback) {
   var resources = scraper.findPrefetchableResources();
+
+  socket.on('response', function(response) {
+    if (!response.url || !response.body)
+      return;
+
+    // server transmitted response body for a URL; notify
+    callback(response.url, response.body);
+  });
+
+  links.forEach(function(link) {
+    // emit a request event for every link on the page
+    socket.emit('request', link);
+  });
 };
